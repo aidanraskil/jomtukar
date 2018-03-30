4337667349cc3827dbcd5cfb19637325e4e78aa7
Iskandar Ali <iskandar_ali@adk.gov.my>
2018-03-30 12:26:00 +0800

    Update front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="jumbotron text-center jumbotron-fluid" style="margin-top: -1.5rem;">
+  <div class="container">
+    <h1 class="display-4 mb-4">Mulanya Di sini</h1>
+    <p class="lead mb-4">Jomtukar merupakan hub kakitangan awam untuk bertukar jabatan atas persetujuan bersama mengikut jawatan, gred dan bidang tugas masing-masing.</p>
+    <a href="#" class="btn btn-primary" style="width: 8rem;">Daftar</a>
+    <a href="#" class="btn btn-outline-primary" style="width: 8rem;">Butiran lanjut</a>
+  </div>
 </div>
 @endsection
