a30280921a52c1d21ca32f78a6f08d477dae9791
Iskandar Ali <github@aidanraskil.bulc.club>
2018-04-03 17:57:58 +0800

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,48 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">	
+<div class="container">
 	<div class="row">
-		<div class="col-md-4">
-			<div class="card">
-				{{-- <div class="" style="background: url('/img/bgimage.svg'); background-size: cover;height: 60px;">
-				</div> --}}
-				<div class="text-center p-4" style="background: url('/img/bgimage.svg'); background-size: cover;">
-					<img class="img-fluid" alt="" src="/img/profilepicture.jpeg" style="border-radius: 50%;border: 5px solid rgba(255,255,255,0.5);">
+		<div class="col-md-3">
+			<div class="card hovercard">
+				<div class="cardheader">
 				</div>
-				<div class="card-body">
-					<h4><strong>{{ $user->name }}</strong></h4>
+				<div class="avatar text-center">
+					<img alt="" src="/img/profilepicture.jpeg">
 				</div>
+				<div class="card-body text-center">
+					<p><strong>{{ $user->name }}</strong></p>
 				@if($user->profiles->count() > 0)
+					<small>
+						{{ $user->profiles->first()->position }} - {{ $user->profiles->first()->grade }} di {{ $user->profiles->first()->office }} <br>
+						{{ $user->profiles->first()->district_from }} &#8226; {{ $user->profiles->first()->statefrom->name }}
+					</small>
+					</div>
 					<ul class="list-group list-group-flush">
-					<li class="list-group-item">
-						<span>Jawatan</span> <span class="float-right">{{ $user->profiles->first()->position }}</span>
-					</li>
-					<li class="list-group-item">
-						<span>Gred</span> <span class="float-right">{{ $user->profiles->first()->grade }}</span>
-					</li>
-					<li class="list-group-item">
-						<span>Lokasi semasa</span> <span class="float-right">{{ $user->profiles->first()->district_from }}, {{ $user->profiles->first()->statefrom->name }}</span>
-					</li>
-					<li class="list-group-item">
-						<span>Lokasi diingini</span> <span class="float-right">{{ $user->profiles->first()->district_to ? $user->profiles->first()->district_to.', ' : '' }} {{ $user->profiles->first()->stateto->name }}</span>
-					</li>
-					{{-- <li class="list-group-item">
-						<i class="flaticon-map-location"></i> {{ $profile->district_from }}, {{ $profile->statefrom->name }}
-					</li>
-					<li class="list-group-item">
-						<i class="flaticon-sign-1"></i> {{ $profile->district_to ? $profile->district_to.', ' : '' }} {{ $profile->stateto->name }}
-					</li> --}}
-				@else
-				 {{dd($user->profiles->contains($user))}}
-				@endif
-					<div class="card-footer">
+						<li class="list-group-item">
+							<span>Jawatan</span> <span class="float-right">{{ $user->profiles->first()->position }}</span>
+						</li>
+						<li class="list-group-item">
+							<span>Gred</span> <span class="float-right">{{ $user->profiles->first()->grade }}</span>
+						</li>
+						<li class="list-group-item">
+							<span>Lokasi semasa</span> <span class="float-right">{{ $user->profiles->first()->district_from }}, {{ $user->profiles->first()->statefrom->name }}</span>
+						</li>
+						<li class="list-group-item">
+							<span>Lokasi diingini</span> <span class="float-right">{{ $user->profiles->first()->district_to ? $user->profiles->first()->district_to.', ' : '' }} {{ $user->profiles->first()->stateto->name }}</span>
+						</li>
+					</ul>
+					<div class="card-footer" style="border-top: none;">
 						<small class="text-muted text-right">Dipos pada {{ $user->profiles->first()->created_at->diffForHumans() }}</small>
 					</div>
-				</ul>
+				@else
+					<p>Profil pertukaran anda tidak dijumpai.</p>
+					<p>Sila klik <a href="{{ route('profile.create') }}" class="text-primary">di sini</a> untuk mencipta profil pertukaran anda.</p>
+				</div>
+				@endif
 			</div>
 		</div>
-		<div class="col-md-8">
+		<div class="col-md-6">
 			<h3 class="text-center">Terbaik Untuk Anda</h3>
 			<hr>
 			@if($best_profiles->count() > 0 )
@@ -51,7 +51,7 @@
 					<div class="col-md-6">
 						<div class="card hovercard mb-4">
 							<div class="cardheader">
-								<span class="badge badge-primary float-right">{{ strtoupper($profile->status) }}</span>				
+								<span class="badge badge-primary float-right">{{ strtoupper($profile->status) }}</span>
 							</div>
 							<div class="avatar">
 								<img alt="" src="/img/profilepicture.jpeg">
@@ -63,7 +63,7 @@
 								<i class="flaticon-map-location"></i> {{ $profile->district_from }}, {{ $profile->statefrom->name }} <br>
 								<i class="flaticon-sign-1"></i> {{ $profile->district_to ? $profile->district_to.', ' : '' }} {{ $profile->stateto->name }}
 								{{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
-							</div>														
+							</div>
 						</div>
 					</div>
 					@endforeach
@@ -84,7 +84,7 @@
 					<div class="col-md-6">
 						<div class="card hovercard mb-4">
 							<div class="cardheader">
-								<span class="badge badge-primary float-right">{{ strtoupper($profile->status) }}</span>				
+								<span class="badge badge-primary float-right">{{ strtoupper($profile->status) }}</span>
 							</div>
 							<div class="avatar">
 								<img alt="" src="/img/profilepicture.jpeg">
@@ -96,7 +96,7 @@
 								<i class="flaticon-map-location"></i> {{ $profile->district_from }}, {{ $profile->statefrom->name }} <br>
 								<i class="flaticon-sign-1"></i> {{ $profile->district_to ? $profile->district_to.', ' : '' }} {{ $profile->stateto->name }}
 								{{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
-							</div>														
+							</div>
 						</div>
 					</div>
 					@endforeach
@@ -109,6 +109,13 @@
 					</div>
 				</div>
 			@endif
+		</div>
+		<div class="col-md-3">
+			<div class="card">
+				<div class="card-body">
+
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
